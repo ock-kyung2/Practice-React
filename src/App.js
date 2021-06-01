8ee2d6f21971e5b80a734425829d4dd2449d4d61
ock-kyung2 <dhrwlsrud94@naver.com>
2021-06-01 17:50:02 +0900

    props와 state 차이 및 state 코드 작성

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Content from './components/Content'
 import Subject from './components/Subject'
 
 class App extends Component {
+
+  constructor(props){
+    // 컴포넌트가 실행될때 render보다 먼저 실행되서 초기화당담  코드  안에 작성한다.
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'world wide web!'}
+    }
+  }
+
   render(){  return (
     <div className="App">
-      <Subject title="WEB" sub="world wide web!"></Subject>
+      <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
       <TOC></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
